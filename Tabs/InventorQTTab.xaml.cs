7029fab13c8c371ac4fe300dbc4cc5aa3e3f8bce
max <maxpayne2024@gmail.com>
2023-04-17 23:01:09 +0200

    Change Excel Interop with EPPLUS library

--- Tabs/InventorQTTab.xaml.cs
+++ Tabs/InventorQTTab.xaml.cs
@@ -128,7 +128,8 @@ namespace SplittableDataGridSAmple.Tabs
         {
             List<DataIQT> fulldata = DataGridQT.dataGridCollection.Where(x=>x.IsVisible == true).SelectMany(data => data.Datas).ToList();
             if (fulldata.Count == 0) return;
-            ExcelHelper.ExportData(fulldata);
+            //ExcelHelper.ExportData(fulldata);
+            EPPlusHelper.ExportData(fulldata);
         }
 
     }
